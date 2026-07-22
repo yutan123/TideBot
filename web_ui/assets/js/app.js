9fe8e37a55ab1b5f2283913edd86192f965a33d8
yutan <qq3438802170@gmail.com>
2026-07-23 00:31:03 +0800

    Add files via upload

--- web_ui/assets/js/app.js
+++ web_ui/assets/js/app.js
@@ -1,22 +1,41 @@
-// Vue Main Application Initialization
-const { createApp } = Vue;
+import Store from './store.js';
+import Navbar from './components/Navbar.js';
+import ConsoleView from './components/ConsoleView.js';
+import ChatView from './components/ChatView.js';
+import APPConnectView from './components/APPConnectView.js';
 
-const app = createApp({
-    setup() {
-        Vue.onMounted(() => {
-            // Render Lucide icons
-            if (window.lucide) window.lucide.createIcons();
-        });
-        return { store };
+// 初始化状态中心
+const store = new Store();
+
+// 初始化导航/侧边栏
+new Navbar(store);
+
+// 视图容器
+const viewContainer = document.getElementById('view-container');
+
+// 路由分发器
+function renderView(viewName) {
+    viewContainer.innerHTML = ''; // 清空当前视图
+
+    switch (viewName) {
+        case 'console':
+            new ConsoleView(viewContainer, store);
+            break;
+        case 'chat':
+            new ChatView(viewContainer, store);
+            break;
+        case 'connect':
+            new APPConnectView(viewContainer, store);
+            break;
+        default:
+            new ConsoleView(viewContainer, store);
     }
+}
+
+// 监听视图切换事件
+store.subscribe('viewChanged', (viewName) => {
+    renderView(viewName);
 });
 
-// Register Components
-app.component('navbar-component', NavbarComponent);
-app.component('console-view', ConsoleView);
-app.component('chat-view', ChatView);
-app.component('app-connect-view', AppConnectView);
-app.component('modal-component', ModalComponent);
-
-// Mount Vue Application
-app.mount('#app');
+// 首屏渲染
+renderView(store.state.currentView);
