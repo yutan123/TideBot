0302b48c618ca293278278ee57fea4d2265d531f
yutan <qq3438802170@gmail.com>
2026-07-23 16:42:57 +0800

    Add files via upload

---
+++ web_ui/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  server: {
+    host: '0.0.0.0', // 支持手机局域网预览
+    port: 3000
+  }
+})
