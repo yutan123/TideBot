6ad89ff4747cf96ec4b2535009301ea6d0d185be
yutan <qq3438802170@gmail.com>
2026-07-23 17:01:40 +0800

    Add files via upload

--- web_ui/vite.config.js
+++ web_ui/vite.config.js
@@ -9,8 +9,15 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src')
     }
   },
+  // 新增打包配置
+  build: {
+    // 假设你的前端文件夹和后端代码在同一个父目录下
+    // 将打包产物直接输出到上层目录的 web_ui 文件夹中
+    outDir: '../web_ui', 
+    emptyOutDir: true // 打包前自动清空旧文件
+  },
   server: {
-    host: '0.0.0.0', // 支持手机局域网预览
+    host: '0.0.0.0',
     port: 3000
   }
 })
