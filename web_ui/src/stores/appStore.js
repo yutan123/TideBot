ca98f4fbab633107439be1d50d932a44a653a139
yutan <qq3438802170@gmail.com>
2026-07-23 16:48:02 +0800

    Add files via upload

--- web_ui/src/stores/appStore.js
+++ web_ui/src/stores/appStore.js
@@ -1,1 +1,28 @@
+import { defineStore } from 'pinia'
 
+export const useAppStore = defineStore('app', {
+  state: () => ({
+    isDarkMode: false,
+    isChatMode: false, // false = 控制台, true = Chat
+    isSidebarOpen: false
+  }),
+  actions: {
+    toggleTheme() {
+      this.isDarkMode = !this.isDarkMode
+      if (this.isDarkMode) {
+        document.documentElement.classList.add('dark')
+      } else {
+        document.documentElement.classList.remove('dark')
+      }
+    },
+    setMode(mode) {
+      this.isChatMode = mode === 'chat'
+    },
+    toggleSidebar() {
+      this.isSidebarOpen = !this.isSidebarOpen
+    },
+    closeSidebar() {
+      this.isSidebarOpen = false
+    }
+  }
+})
